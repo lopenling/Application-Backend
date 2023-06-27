b2a5872c0907ec9500eb19c523b1e23ac393b665
lobsam <sanduplobzang@gmail.com>
2023-06-27 18:32:17 +0530

    small change

--- hasura-db-migrations/migrations/padma-dictionary/1684738241830_insert_into_data_language/up.sql
+++ hasura-db-migrations/migrations/padma-dictionary/1684738241830_insert_into_data_language/up.sql
@@ -1,1 +1,1 @@
-INSERT INTO "data"."language"("value", "comment") VALUES (E'Tibetan', null);
+INSERT INTO "data"."language"("value", "comment") VALUES (E'Tibetan', null) ON CONFLICT ("value") DO NOTHING;
