bfc7efda1d54c2ed665c405e910abbdc0b387775
lobsam <sanduplobzang@gmail.com>
2023-07-18 18:59:29 +0530

    add Hausura jwt token

--- setup-prod/setup-prod.sh
+++ setup-prod/setup-prod.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
 source .env-prod
-
-# install depdencies
-pip install git+https://github.com/Lotus-King-Research/Padma-Dictionary-Lookup
-pip install tqdm
-
-# build the dictionary sql files
-python ../dictionaries-etl/dictionaries_to_sql.py
-
 ## Container Name as specified in docker-compose.yml
 $DOCKER_CONTAINER_NAME="hasura/graphql-engine:v2.3.0"
 
 ## Start/Restart docker container
 docker-compose rm -svf $DOCKER_CONTAINER_NAME;
-
-# Removes db volume
-sudo docker volume rm -f padma-dictionary-service_db_data;
 
 docker-compose up -d $DOCKER_CONTAINER_NAME --force-recreate
 
@@ -28,12 +17,3 @@ cd ../hasura-db-migrations;
 hasura metadata apply --endpoint $DEV_HASURA_ENDPOINT --admin-secret $HASURA_REMOTE_SERVER_ADMIN_SECRET
 hasura migrate apply --all-databases --endpoint $DEV_HASURA_ENDPOINT --admin-secret $HASURA_REMOTE_SERVER_ADMIN_SECRET
 hasura metadata reload --endpoint $DEV_HASURA_ENDPOINT --admin-secret $HASURA_REMOTE_SERVER_ADMIN_SECRET
-cd -;
-
-CONTAINER_ID=$(docker ps | grep postgres:12 | grep setup-prod_postgres_1 | cut -d ' ' -f1)
-
-docker exec -i $CONTAINER_ID psql -h localhost -U postgres -d postgres < words.sql;
-docker exec -i $CONTAINER_ID psql -h localhost -U postgres -d postgres < descriptions.sql;
-
-rm words.sql
-rm descriptions.sql
