616accbee4bc494479d39acc40bcf93124d182d6
Margus Kevin Sünter <kevin@synter.ee>
2024-06-01 00:50:11 +0300

    Add dictionaries managmeent

--- app/controllers/dictionaries_controller.ts
+++ app/controllers/dictionaries_controller.ts
@@ -1,28 +1,50 @@
+import Dictionary from '#models/dictionary'
+import { createDictionaryValidator, updateDictionaryValidator } from '#validators/dictionary'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DictionariesController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({}: HttpContext) {
+    return Dictionary.all()
+  }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request }: HttpContext) {
+    const validated = await request.validateUsing(createDictionaryValidator)
+
+    return Dictionary.create(validated)
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params }: HttpContext) {
+    return Dictionary.findByOrFail(params.id)
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request }: HttpContext) {
+    const validated = await request.validateUsing(updateDictionaryValidator)
+    const dictionary = await Dictionary.findOrFail(params.id)
+
+    dictionary.name = validated.name
+
+    await dictionary.save()
+
+    return dictionary
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params }: HttpContext) {
+    const dictionary = await Dictionary.findOrFail(params.id)
+    return dictionary.delete()
+  }
 }
