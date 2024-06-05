6f07ed2969beae1268ed3304498f67008fe87ed0
Margus Kevin Sünter <kevin@synter.ee>
2024-06-05 16:30:05 +0300

    Insert .csv dictionaries to DB

--- app/models/dictionary.ts
+++ app/models/dictionary.ts
@@ -5,6 +5,7 @@ export default class Dictionary extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
+  @column()
   declare name: string
 
   @column.dateTime({ autoCreate: true })
