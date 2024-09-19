d90a4b57109e53c96b27efbdbc75dffa884a47be
Margus Kevin Sünter <kevin@synter.ee>
2024-09-19 14:14:28 +0300

    Add timestamps to team model

--- app/models/team.ts
+++ app/models/team.ts
@@ -1,6 +1,7 @@
 import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
 import User from './user.js'
 import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+import { DateTime } from 'luxon'
 
 export default class Team extends BaseModel {
   /**
@@ -23,4 +24,10 @@ export default class Team extends BaseModel {
     pivotTimestamps: true,
   })
   declare users: ManyToMany<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
 }
